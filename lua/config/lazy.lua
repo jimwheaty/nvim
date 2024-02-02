93dbd5de6fd94353e5d0043f64c05666787144e5
d_starantzis <dimitris@sundaygroupinc.com>
2024-02-02 13:18:04 +0200

    Disabled some unnecessary plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -2,14 +2,15 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim",                                    import = "lazyvim.plugins" },
     -- import any extras modules here
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.coding.tabnine" },
@@ -26,7 +27,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "plugins" },
   },
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,5 @@
+return {
+  { "stevearc/conform.nvim",     enabled = false },
+  { "echasnovski/mini.pairs",    enabled = false },
+  { "echasnovski/mini.surround", enabled = false },
+}
