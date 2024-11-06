3e99e052c8067d481b036e1fe9490da0717a5364
jimwheaty <d.starantzis97@gmail.com>
2024-11-06 16:10:21 +0200

    late 2024

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,6 @@
 return {
-  { "stevearc/conform.nvim",     enabled = false },
-  { "echasnovski/mini.pairs",    enabled = false },
-  { "echasnovski/mini.surround", enabled = false },
+  { "stevearc/conform.nvim",               enabled = false },
+  { "echasnovski/mini.pairs",              enabled = false },
+  { "echasnovski/mini.surround",           enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
 }
--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -3,10 +3,10 @@ return {
     "christoomey/vim-tmux-navigator",
     lazy = false,
     keys = {
-      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>", desc = "window left" },
+      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>",  desc = "window left" },
       { "<C-l>", "<cmd>TmuxNavigateRight<cr>", desc = "window right" },
-      { "<C-j>", "<cmd>TmuxNavigateDown", "window down" },
-      { "<C-k>", "<cmd>TmuxNavigateUp", "window up" },
+      { "<C-j>", "<cmd>TmuxNavigateDown",      "window down" },
+      { "<C-k>", "<cmd>TmuxNavigateUp",        "window up" },
     },
   },
 }
