88c1bf821240c0b543154dbd56162bcc55ebdefe
jimwheaty <d.starantzis97@gmail.com>
2025-04-24 21:08:29 +0300

    end of april 2025

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -29,6 +29,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.editor.telescope" },
     { import = "plugins" },
   },
   defaults = {
